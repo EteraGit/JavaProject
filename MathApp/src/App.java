23beaade9d5dd7598d2390f6b28b2bdeaced1fff
zana55 <zanaperkovic545@gmail.com>
2022-10-21 12:02:03 +0200

    nekakav koordinatni sustav

--- MathApp/src/App.java
+++ MathApp/src/App.java
@@ -1,7 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class App {
 
     public static void main(String[] args) throws Exception 
     {
-        new LaunchPage();
+        JLabel l1 = new JLabel("y");
+        l1.setFont(new Font("Serif", Font.PLAIN, 30));
+        l1.setBounds(260, 0, l1.getPreferredSize().width, l1.getPreferredSize().height);
+
+        JLabel l2 = new JLabel("x");
+        l2.setFont(new Font("Serif", Font.PLAIN, 30));
+        l2.setBounds(450, 400, l2.getPreferredSize().width, l2.getPreferredSize().height);
+        
+        
+        Panel panel = new Panel();
+        System.out.println("Hello world!");
+        JFrame Prozor = new JFrame();
+        
+        Prozor.setSize(500, 500);
+        Prozor.setLocationRelativeTo(null);
+        Prozor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Prozor.setResizable(false);
+        Prozor.add(panel);
+        Prozor.add(l1);
+        Prozor.add(l2);
+    
+        Prozor.setVisible(true);
+
+
     }
+
 }
